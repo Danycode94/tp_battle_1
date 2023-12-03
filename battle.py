62ef84603b1377737c694bc97ad6c422d6076408
badio dany <badiodany11@gmail.com>
2023-12-02 16:53:44 -0800

    commit

--- battle.py
+++ battle.py
@@ -1,7 +1,20 @@
 import re
 
-chenn = "badio ap challenge 02 0  dec 2023"
-
+chenn = "badio ap challenge 02 0 12 1  dec 2023"
+# re.match("/")
 n = re.findall("\d+", chenn)
 
 print(n)
+
+# lisdijit='0,1,2,3,4,5,6,7,8,9'
+# chen="idiot il est la 01 kay 07 089"
+# rezilta=[]
+# j=0
+# for i in chen:
+#     j+=1
+#     if(i=="0"):
+#         print(j)
+#         if(chen[j] in lisdijit and j-2!=0):
+#             rezilta.append(f'{chen[j]}')
+
+# print(rezilta)
